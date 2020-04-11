c91c0a9a45a41c51c9246a336e155904f0175e3b
Alex Albright <alexanderjalbright@gmail.com>
2020-04-11 16:10:57 -0400

    Added MatUI navbar, added logo, made search use google, and user icon shows user details and has functional logout button

--- frontend/src/app/userDashboard/navbar/Navbar.js
+++ frontend/src/app/userDashboard/navbar/Navbar.js
@@ -1,29 +1,312 @@
-import React, { useState } from "react";
-import styles from "./Navbar.module.css";
+// import React, { useState } from "react";
+// import styles from "./Navbar.module.css";
 
-export const Navbar = ({ user }) => {
-  const [isFlapping, setIsFlapping] = useState(false);
+// export const Navbar = ({ user }) => {
+//   const [isFlapping, setIsFlapping] = useState(false);
+//   return (
+//     <div className={styles.navbar}>
+//       <div className={styles.name}>{user.name}</div>
+//       <div
+//         className={styles.logo}
+//         onMouseEnter={() => setIsFlapping(true)}
+//         onMouseLeave={() => setIsFlapping(false)}
+//       >
+//         mot
+//         <span
+//           className={`${styles.moth} ${isFlapping && styles.leftWingFlap}`}
+//         >{`}`}</span>
+//         <span
+//           className={`${styles.moth} ${isFlapping && styles.mothBody}`}
+//         >{`;`}</span>
+//         <span
+//           className={`${styles.moth} ${isFlapping && styles.rightWingFlap}`}
+//         >{`{`}</span>
+//         ra
+//       </div>
+//       <div className={styles.settings}>@</div>
+//     </div>
+//   );
+// };
+
+import React, { useState, useEffect, useRef } from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
+
+import { Logo } from "./logo";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+    gridArea: "navbar",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+}));
+
+export const Navbar = ({ user, setUser, setPageSelected }) => {
+  // ALEX TESTING
+  // use for testing notifications and messages badges
+  useEffect(() => {
+    setUser({
+      ...user,
+      messages: [
+        "You're fired.",
+        "Did I mention you are fired?",
+        "In case you missed it, you no longer work here.",
+      ],
+      notifications: ["Status Changed to: FIRED"],
+    });
+  }, []);
+  // ALEX TESTING
+  const [search, setSearch] = useState("");
+  const searchAnchor = useRef(null);
+
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setAnchorEl(null);
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = (action) => {
+    switch (action) {
+      case "logout":
+        setPageSelected("login");
+        setUser(null);
+        break;
+      case "default":
+        break;
+    }
+
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem>{user.name}</MenuItem>
+      <MenuItem>{user.email}</MenuItem>
+      <hr />
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={() => handleMenuClose("logout")}>Logout</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new messagess" color="inherit">
+          <Badge badgeContent={user.messages?.length} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={user.notifications?.length} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
-    <div className={styles.navbar}>
-      <div className={styles.name}>{user.name}</div>
-      <div
-        className={styles.logo}
-        onMouseEnter={() => setIsFlapping(true)}
-        onMouseLeave={() => setIsFlapping(false)}
-      >
-        mot
-        <span
-          className={`${styles.moth} ${isFlapping && styles.leftWingFlap}`}
-        >{`}`}</span>
-        <span
-          className={`${styles.moth} ${isFlapping && styles.mothBody}`}
-        >{`;`}</span>
-        <span
-          className={`${styles.moth} ${isFlapping && styles.rightWingFlap}`}
-        >{`{`}</span>
-        ra
-      </div>
-      <div className={styles.settings}>@</div>
+    <div className={`${classes.grow}`}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            <Logo />
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Google Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                  searchAnchor.current.click();
+                }
+              }}
+            />
+            <a
+              href={"https://www.google.com/search?&q=" + search}
+              target="_blank"
+              rel="noopener"
+              ref={searchAnchor}
+            />
+          </div>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new messagess" color="inherit">
+              <Badge badgeContent={user.messages?.length} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge
+                badgeContent={user.notifications?.length}
+                color="secondary"
+              >
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
     </div>
   );
 };

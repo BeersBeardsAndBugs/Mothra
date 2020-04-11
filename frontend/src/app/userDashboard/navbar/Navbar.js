917f735ed3480eda255b72681ac8d5f5735ee4f1
Alex Albright <alexanderjalbright@gmail.com>
2020-04-11 14:19:49 -0400

    Tweaked logo animation

--- frontend/src/app/userDashboard/navbar/Navbar.js
+++ frontend/src/app/userDashboard/navbar/Navbar.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 
 export const Navbar = ({ user }) => {
+  const [isFlapping, setIsFlapping] = useState(false);
   return (
     <div className={styles.navbar}>
       <div className={styles.name}>{user.name}</div>
-      <div className={styles.logo}>
-        mot<span className={styles.leftWing}>{`}`}</span>;
-        <span className={styles.rightWing}>{`{`}</span>ra
+      <div
+        className={styles.logo}
+        onMouseEnter={() => setIsFlapping(true)}
+        onMouseLeave={() => setIsFlapping(false)}
+      >
+        mot
+        <span
+          className={`${styles.moth} ${isFlapping && styles.leftWingFlap}`}
+        >{`}`}</span>
+        <span
+          className={`${styles.moth} ${isFlapping && styles.mothBody}`}
+        >{`;`}</span>
+        <span
+          className={`${styles.moth} ${isFlapping && styles.rightWingFlap}`}
+        >{`{`}</span>
+        ra
       </div>
       <div className={styles.settings}>@</div>
     </div>

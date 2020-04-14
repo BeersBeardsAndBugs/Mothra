8ff600118d424e95dfcd56e356151e0e33275228
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-04-14 13:20:04 -0500

    Added OnClick logic for BugList bug clicks

--- frontend/src/app/userDashboard/BugList/BugList.js
+++ frontend/src/app/userDashboard/BugList/BugList.js
@@ -1,6 +1,4 @@
 import React from "react";
-import styles from "./BugList.module.css";
-
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -8,21 +6,14 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import BugReportIcon from '@material-ui/icons/BugReportRounded';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-  },
-  demo: {
+
+  matTheme: {
     backgroundColor: theme.palette.background.paper,
-  },
-  title: {
-    margin: theme.spacing(4, 0, 2),
-  },
+  }
 }));
 
 
@@ -73,13 +64,16 @@ export const BugList = ({ bugs2 }) => {
     }
   ]
 
+    function handleClick(bug) {
+      console.log(bug);
+    }
+
   return (
-    <div className={classes.root}>
-      <div className={classes.demo}>
+      <div className={classes.matTheme}>
         <List>
           {
             bugs.map((bug) => (
-              <ListItem>
+              <ListItem button onClick={() =>handleClick(bug)}>
                 <ListItemAvatar>
                   <Avatar style={
                     bug.severity === "blocker" ? {background: "#b71c1c"} : 
@@ -97,15 +91,19 @@ export const BugList = ({ bugs2 }) => {
                   secondary={bug.owner ? `${bug.owner}` : "No Owner"}
                 />
                 <ListItemSecondaryAction>
+                {/*
+                  Keeping this here in case we want to include something in future
+
                   <IconButton edge="end" aria-label="delete">
                     <DeleteIcon />
                   </IconButton>
+
+                */}
                 </ListItemSecondaryAction>
               </ListItem>
             ))
           }
         </List>
       </div>
-    </div>
   );
 }

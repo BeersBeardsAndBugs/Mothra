53fc4dba60f0307e90724cdfe9158202b4195ab3
Alex Albright <alexanderjalbright@gmail.com>
2020-04-29 19:34:42 -0400

    Added bugId to new comment, and userEmail to editted bug

--- frontend/src/app/userDashboard/BugDetail/comments/Comments.js
+++ frontend/src/app/userDashboard/BugDetail/comments/Comments.js
@@ -15,7 +15,7 @@ export const Comments = ({ comments, bugId }) => {
     const [newComment, setNewComment] = useState('')
 
     const handleSubmit = () => {
-        comments.add({ text: newComment })
+        comments.add({ text: newComment, bugId })
     }
 
     return (

8a2c167b3babb1d399045c3f922e8b1db730eca9
PrestonWestDev <57919582+PrestonWestDev@users.noreply.github.com>
2020-05-02 11:42:36 -0500

    Updated Modal (Actually a Dialog) UI
    WIP, more work left to to on logic side

--- frontend/src/app/userDashboard/newBugModal/NewBugModal.js
+++ frontend/src/app/userDashboard/newBugModal/NewBugModal.js
@@ -1,32 +1,287 @@
-import Modal from '@material-ui/core/Modal'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 import { NewBug } from './newBug'
 
+import FilledInput from '@material-ui/core/FilledInput';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import Checkbox from '@material-ui/core/Checkbox'
+import ListItemText from '@material-ui/core/ListItemText'
+
+import Avatar from '@material-ui/core/Avatar'
+import BugReportIcon from '@material-ui/icons/BugReportRounded'
+
+import Box from '@material-ui/core/Box'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import CloseIcon from '@material-ui/icons/Close'
+import SettingsOverscanIcon from '@material-ui/icons/SettingsOverscan'
+import { positions } from '@material-ui/system';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+
+
+
+import { BUG, PATH } from '../../../constants'
+
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: 'absolute',
-        width: 400,
+
+    pictureContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        overflowY: 'hidden',
+        overflowX: 'hidden',
+    },
+    gridList: {
+        flexWrap: 'nowrap',
+        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+        transform: 'translateZ(0)',
+    },
+    forceInline : {
+        display: 'inline-block',
+        margin: 'auto',
+    },
+    Blocker: {
+        background: '#b71c1c',
+    },
+    Critical: {
+        background: '#e53935',
+    },
+    High: {
+        background: '#fb8c00',
+    },
+    Normal: {
+        background: '#fdd835',
+    },
+    Enhancement: {
+        background: '#8bc34a',
     },
 }))
 
+
+
 export const NewBugModal = ({ isNewBugModalOpen, handleNewBugModalClose }) => {
-    const classes = useStyles()
+    const classes = useStyles();
+
+
+    const inputsSchema = {
+        [BUG.DESCRIPTION]: {
+            value: '',
+            error: '',
+            name: BUG.DESCRIPTION,
+            required: true,
+        },
+    }
+
+    const pictures = [
+        { img: 'jpg_img.JPG', title: '.jpg upload', author: 'Preston' },
+        { img: 'gif_img.gif', title: '.gif upload', author: 'Jordon' },
+        { img: 'ultrawide_img.png', title: 'ultrawide upload', author: 'Alex' },
+        { img: 'png_img.png', title: '.png upload', author: 'Alex' },
+    ]
+
+
+
+    const [title, setTitle] = React.useState('');
+    const [desc, setDesc] = React.useState('');
+    const [tags, setTags] = React.useState([]);
+    const [priority, setPriority] = React.useState('None');
+    const [assignTo, setAssignTo] = React.useState('');
+    const [isSubmitDisabled, setIsSubmitDisabled] = React.useState([true]);
+    const [handleSubmit, setHandleSubmit] = React.useState([]);
+    const names = [
+        'Jordon West',
+        'Preston West',
+        'Alex Albright',
+      ];
+    const priorities = [
+        'Blocker',
+        'Critical',
+        'High',
+        'Normal',
+        'Enhancement',
+        'None'
+      ];
+
+    const handleTagsChange = (event) => {
+        setTags(event.target.value);
+    };
+
+    const handlePriorityChange = (event) => {
+        setPriority(event.target.value);
+    };
+
+    
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleDescChange = (event) => {
+        setDesc(event.target.value);
+    };
+
+    const handleAssignToChange = (event) => {
+        setAssignTo(event.target.value);
+    };
 
     return (
-        <div>
-            <Modal
-                open={isNewBugModalOpen}
-                onClose={handleNewBugModalClose}
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-            >
-                <NewBug {...{ classes }} />
-            </Modal>
+        <div className={classes.root}>
+            <Dialog maxWidth='lg' fullWidth open={isNewBugModalOpen} disableBackdropClick onClose={handleNewBugModalClose}>
+            <DialogTitle>
+
+            <Grid container direction="row" justify="space-between" alignItems="center" spacing={2}>
+                <Grid item>
+                    <Typography>Create New Ticket</Typography>
+                </Grid>
+                <Grid item justify='flex-end'>
+                    <IconButton onClick={handleNewBugModalClose}>
+                        <CloseIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
+
+            </DialogTitle>
+            <DialogContent>
+            <form noValidate autoComplete="off">
+            <Grid container direction="row" justify="center" alignItems="stretch" spacing={2}>
+                <Grid item xs={12}>
+                    <FormControl fullWidth variant="outlined">
+                        <InputLabel htmlFor="component-outlined">Bug title</InputLabel>
+                        <OutlinedInput autoFocus id="component-outlined" value={title} onChange={handleTitleChange} label="Bug Title" />
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={7} >
+                    <TextField
+                        fullWidth
+                        id="standard-textarea"
+                        label="Description"
+                        placeholder="Description"
+                        multiline
+                        rows={4}
+                        name="Description"
+                        value={desc}
+                        onChange={handleDescChange}
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={12} sm={5}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-mutiple-name-label">Priority</InputLabel>
+                    <Select value={priority} onChange={handlePriorityChange} input={<Input />}>
+                    {priorities.map((priority) => (
+                        <MenuItem key={priority} value={priority}>
+                            <ListItemAvatar className={classes.forceInline}>
+                                <Avatar variant='square' className={classes[priority]}>
+                                    <BugReportIcon></BugReportIcon>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={priority} className={classes.forceInline} />
+                        </MenuItem>))}
+                    </Select>
+                </FormControl>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-mutiple-name-label">Assign To</InputLabel>
+                    <Select value={assignTo} onChange={handleAssignToChange} input={<Input />}>
+                    {names.map((name) => (
+                        <MenuItem key={name} value={name}>
+                            <ListItemAvatar className={classes.forceInline}>
+                                <Avatar variant='square' alt="" src={name.split(" ")[0] +'.jpg'}></Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={name} className={classes.forceInline} />
+                        </MenuItem>))}
+                    </Select>
+                </FormControl>
+
+                <FormControl fullWidth >
+                    <InputLabel>Notify Teammates</InputLabel>
+                    <Select multiple value={tags} onChange={handleTagsChange} input={<Input />} 
+                    renderValue={(selected) => selected.join(', ')}>
+                        {names.map((name) => (
+                            <MenuItem key={name} value={name}>
+                                <ListItemAvatar className={classes.forceInline}>
+                                    <Avatar variant='square' alt="" src={name.split(" ")[0] +'.jpg'}></Avatar>
+                                </ListItemAvatar>
+                            <ListItemText primary={name} />
+                            <Checkbox checked={tags.indexOf(name) > -1} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+                </Grid>
+
+            {/*   Bug Detail Images   */}
+            <Grid item xs={12} sm={12}>
+                <Card>
+                    <CardContent>
+
+                    <Button type="submit" variant="contained" color="secondary">
+                        + Add Image
+                    </Button>
+
+{/*** 
+
+                        <div className={classes.pictureContainer}>
+                            <GridList cols={2.5} className={classes.gridList}>
+                                {pictures.map((tile) => (
+                                    <GridListTile key={tile.img}>
+                                        <img src={tile.img} alt={tile.title} />
+                                        <GridListTileBar title={tile.title}
+                                        actionIcon={
+                                            <IconButton aria-label={`star ${tile.title}`}>
+                                                <SettingsOverscanIcon/>
+                                            </IconButton>
+                                            }
+                                        />
+                                    </GridListTile>
+                                ))}
+
+                                <GridListTile>
+                                <GridListTileBar title="Add Image">
+
+                                </GridListTileBar>
+                                    
+                                </GridListTile>
+                            </GridList>
+                        </div>
+
+*/}
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+                <Button type="submit" variant="contained" color="primary" onClick={handleSubmit} disabled={isSubmitDisabled || false}>
+                    Save Change
+                </Button>
+            </Grid>
+            </Grid>
+            </form>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
--- frontend/src/app/userDashboard/newBugModal/newBug/NewBug.js
+++ frontend/src/app/userDashboard/newBugModal/newBug/NewBug.js
@@ -1,15 +1,239 @@
 import React from 'react'
+import FilledInput from '@material-ui/core/FilledInput';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import IconButton from '@material-ui/core/IconButton'
+import SettingsOverscanIcon from '@material-ui/icons/SettingsOverscan'
 
 export const NewBug = ({ classes }) => {
-    const [modalStyle] = React.useState({
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-    })
 
-    return (
-        <div style={modalStyle} className={classes.paper}>
-            this is a new bug
-        </div>
-    )
+  const useStyles = makeStyles((theme) => ({
+    // CSS Trick to handle Material Defect that causes horizontal overflow on Grid spacing
+    // We'll use this until Google decides to fix the issue. Tho its been around for over a year...
+    grid: {
+      margin: theme.spacing(0),
+      flexGrow: 0,
+      maxWidth: `100%`,
+      flexBasis: `100%`
+  },
+    detailsContainer : {
+      minHeight: '300px',
+    },
+    title: {
+        color: theme.palette.primary.light,
+    },
+    titleBar: {
+        background:
+            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    pictureContainer: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      backgroundColor: theme.palette.background.paper,
+      height: '300px',
+      overflowY: 'auto',
+      overflowX: 'hidden',
+  },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  }));
+
+  const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+const theme = useTheme();
+  const [dummy, setDummy] = React.useState('Dummy Value');
+  const names = [
+    'Jordon West',
+    'Preston West',
+    'Alex Albright',
+  ];
+
+  const [assignTo, setAssignTo] = React.useState([]);
+
+  const [modalStyle] = React.useState({
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  })
+
+  const handleChange = (event) => {
+    setDummy(event.target.value);
+  };
+
+  function getStyles(name, personName, theme) {
+    return {
+      fontWeight:
+        personName.indexOf(name) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+  return (
+
+    
+      <div style={modalStyle} className={classes.paper}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Card>
+              <CardContent>
+                <Typography variant="h1">
+                  Create New Bug
+                </Typography>
+                <br />
+                <Typography variant="subtitle1">
+                  ID: none
+                </Typography>
+                </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={7} >
+                <Card >
+                    <CardContent > 
+                    <div className={classes.detailsContainer}>
+                        <TextField
+                            fullWidth
+                            id="standard-textarea"
+                            label="Description"
+                            placeholder="Description"
+                            multiline
+                            rows={4}
+                            name="Description"
+                            value={''}
+                            onChange={handleChange}
+                            variant="outlined"
+                        />
+                    </div>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+
+    {/*
+      <form className={classes.root} noValidate autoComplete="off">
+        <FormControl fullWidth variant="outlined">
+          <InputLabel htmlFor="component-outlined">Bug title</InputLabel>
+          <OutlinedInput  id="component-outlined" value={''} onChange={handleChange} label="Bug Title" />
+        </FormControl>
+        <FormControl>
+        <InputLabel id="demo-mutiple-name-label">Assign To</InputLabel>
+        <Select
+          labelId="demo-mutiple-name-label"
+          id="demo-mutiple-name"
+          multiple
+          value={assignTo}
+          onChange={handleChange}
+          input={<Input />}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem key={name} value={name} style={getStyles(name, assignTo, theme)}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel id="demo-mutiple-name-label">Assign To</InputLabel>
+        <Select
+          labelId="demo-mutiple-name-label"
+          id="demo-mutiple-name"
+          multiple
+          value={assignTo}
+          onChange={handleChange}
+          input={<Input />}
+          MenuProps={MenuProps}
+        >
+          {names.map((name) => (
+            <MenuItem key={name} value={name} style={getStyles(name, assignTo, theme)}>
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+
+        <FormControl variant="outlined">
+          <InputLabel htmlFor="component-outlined">Description</InputLabel>
+          <OutlinedInput id="component-outlined" value={''} onChange={handleChange} label="Bug Description" />
+        </FormControl>
+
+        <FormControl>
+          <InputLabel htmlFor="component-helper">Name</InputLabel>
+          <Input
+            id="component-helper"
+            value={dummy}
+            onChange={handleChange}
+            aria-describedby="component-helper-text"
+          />
+          <FormHelperText id="component-helper-text">Some important helper text</FormHelperText>
+        </FormControl>
+        <FormControl disabled>
+          <InputLabel htmlFor="component-disabled">Name</InputLabel>
+          <Input id="component-disabled" value={dummy} onChange={handleChange} />
+          <FormHelperText>Disabled</FormHelperText>
+        </FormControl>
+        <FormControl error>
+          <InputLabel htmlFor="component-error">Name</InputLabel>
+          <Input
+            id="component-error"
+            value={dummy}
+            onChange={handleChange}
+            aria-describedby="component-error-text"
+          />
+          <FormHelperText id="component-error-text">Error</FormHelperText>
+        </FormControl>
+
+        <FormControl variant="filled">
+          <InputLabel htmlFor="component-filled">Name</InputLabel>
+          <FilledInput id="component-filled" value={dummy} onChange={handleChange} />
+        </FormControl>
+      </form>
+    
+*/}
+
+</Grid>
+    </div>
+  
+  )
 }

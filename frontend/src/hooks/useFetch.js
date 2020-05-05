a8a51be029daef5410a3528bb9eb72c378b312ad
Alex Albright <alexanderjalbright@gmail.com>
2020-05-04 21:05:47 -0400

    Removed optimistic update on add, altered how useFetch response is handled

--- frontend/src/hooks/useFetch.js
+++ frontend/src/hooks/useFetch.js
@@ -8,7 +8,9 @@ const dataReducer = (objects, { type, payload }) => {
             return objects.filter((object) => object.id !== payload)
         case 'edit':
             return objects.map((object) =>
-                object.id === payload.id ? { ...object, ...payload } : object
+                object['id'] === payload['id']
+                    ? { ...object, ...payload }
+                    : object
             )
         case 'replace':
             return payload
@@ -26,9 +28,6 @@ export const useFetch = (basePath, defaultResponse = null) => {
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
     const responseRef = useRef()
-    // useEffect(() => {
-
-    // })
 
     const handleError = (error) => {
         setError(error)
@@ -41,6 +40,7 @@ export const useFetch = (basePath, defaultResponse = null) => {
     const dataFetch = async (method, pathExtention = '', options) => {
         const headers = options['headers']
         const body = options['body']
+        const type = options['type']
         setIsLoading(true)
         try {
             let options = {
@@ -65,8 +65,8 @@ export const useFetch = (basePath, defaultResponse = null) => {
             } else {
                 const json = await res.json()
                 console.log('DATA', json)
-                if (method === 'get' || (method === 'post' && pathExtention)) {
-                    dispatchResponse({ type: 'replace', payload: json })
+                if (type) {
+                    dispatchResponse({ type, payload: json })
                 }
             }
         } catch (err) {
@@ -81,30 +81,31 @@ export const useFetch = (basePath, defaultResponse = null) => {
     }
 
     const getAll = (_headers) => {
-        dataFetch('get', '', { headers: _headers })
+        dataFetch('get', '', { headers: _headers, type: 'replace' })
     }
 
     const special = (path, body) => {
-        dataFetch('post', path, { body })
+        dataFetch('post', path, { body, type: 'replace' })
     }
 
     const add = (body) => {
         responseRef.current = response
-        dispatchResponse({ type: 'add', payload: body })
-        dataFetch('post', '', { body })
+        // dispatchResponse({ type: 'add', payload: body }) // removed optimistic update for add
+        dataFetch('post', '', { body, type: 'add' })
     }
 
     const getById = (id) => {
-        console.log('NOTIFICATIONS')
-        dataFetch('get', `/${id}`, { headers: {}, body: {} })
+        dataFetch('get', `/${id}`, { type: 'replace' })
     }
 
     const edit = (body) => {
+        responseRef.current = response
         dispatchResponse({ type: 'edit', payload: body })
         dataFetch('put', `/${body.id}`, { body })
     }
 
     const remove = (id) => {
+        responseRef.current = response
         dispatchResponse({ type: 'remove', payload: id })
         dataFetch('delete', `/${id}`, {})
     }
